82fa2d4d29cb866205f96ebc03eb6b347942ed1e
chrcar01 <chrisjcarter@gmail.com>
2023-07-29 11:00:06 -0700

    Added Graphviz sample app

---
+++ samples/GraphvizDemo/GraphvizDotWrapper.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+
+namespace GraphvizDemo
+{
+    /// <summary>
+    /// Wrapper around the Graphviz dot executable.
+    /// </summary>
+    public class GraphvizDotWrapper
+    {
+        private readonly string _dotExePath;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GraphvizDotWrapper"/> class.
+        /// </summary>
+        /// <param name="dotExePath">Fully qualified path to the dot.exe executable e.g. C:\Program Files\Graphviz\bin\dot.exe</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public GraphvizDotWrapper(string dotExePath)
+        {
+            _dotExePath = dotExePath ?? throw new ArgumentNullException(nameof(dotExePath));
+        }
+
+        public byte[] Render(string dotString, string? format = "svg")
+        {
+            if (!new[] { "svg", "pdf", "jpg", "gif" }.Contains(format, StringComparer.OrdinalIgnoreCase))
+            {
+                throw new ArgumentException($"Invalid format: {format}", nameof(format));
+            }
+
+            var dotFile = Path.GetTempFileName();
+            File.WriteAllText(dotFile, dotString);
+            var outputFile = Path.GetTempFileName();
+
+            var process = new Process();
+            // Stop the process from opening a new window
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+
+            // Setup exe and params
+            process.StartInfo.FileName = _dotExePath;
+            process.StartInfo.Arguments = $@" -T{format} {dotFile}  -o {outputFile}";
+
+            // Go!
+            process.Start();
+
+            // Wait for process to finish
+            process.WaitForExit();
+
+            // Read the diagram from the temp file
+            return File.ReadAllBytes(outputFile);
+        }
+    }
+}
---
+++ samples/GraphvizDemo/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Diagnostics;
+using CC.SolutionsAnalyzer;
+
+namespace GraphvizDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var solutionFileParser = new SolutionFileParser();
+
+        // Get all solutions we want to map dependencies for
+        var solutionFiles = new[]
+        {
+            @"D:\source\svn\ARS\Omnis\ErpTaskRunner.sln",
+            @"D:\source\svn\ARS\Omnis\OMNIS.sln",
+            @"D:\source\svn\ARS\Omnis\UmlerEhmsTaskRunner.sln"
+        };
+        var solutions = solutionFiles.Select(solutionFileParser.GetSolution);
+
+        // Alternatively, you can use the solution file parser to get all solutions in a directory:
+        // var solutions = solutionFileParser.GetSolutionsInDirectory(@"D:\source\svn\ARS\Omnis");
+
+        // Create a graphviz dot string, https://graphviz.org/doc/info/lang.html
+        var dotString = CreateSolutionsAndProjectsDependenciesDotString(solutions);
+        // Test out the dot string here: https://dreampuf.github.io/GraphvizOnline/
+
+        // make sure you have graphviz installed: https://graphviz.org/download/
+        var dotExePath = @"C:\Program Files\Graphviz\bin\dot.exe";
+        var dot = new GraphvizDotWrapper(dotExePath);
+
+        // Render the dot string to a byte array
+        var svg = dot.Render(dotString, format: "svg");
+
+        // Save the byte array to a file
+        var diagramSvgFilePath = @"D:\output.svg";
+        File.WriteAllBytes(diagramSvgFilePath, svg);
+
+        // Open the file
+        OpenWithDefaultProgram(diagramSvgFilePath);
+    }
+
+    static string CreateSolutionsAndProjectsDependenciesDotString(IEnumerable<VisualStudioSolution> solutions)
+    {
+        var allProjects = solutions
+            .SelectMany(s => s.Projects)
+            .GroupBy(p => p.Name)
+            .Select(g => g.First());
+
+        using var text = new StringWriter();
+        text.WriteLine("digraph Dependencies {");
+        text.WriteLine("    rankdir=LR;");
+        text.WriteLine("    node[shape = rect];");
+        foreach (var sol in solutions)
+        {
+            text.WriteLine($"   \"{sol.Name}.sln\" [shape=circle,style=filled,color=red]");
+            foreach (var p in sol.Projects)
+            {
+                text.WriteLine($"   \"{sol.Name}.sln\" -> \"{p.Name}.csproj\"");
+                if (!p.ReferencedProjects.Any()) continue;
+                    
+                foreach (var projectName in p.ReferencedProjects.Select(x => x.Name))
+                {
+                    var dep = allProjects.FirstOrDefault(proj => proj.Name == projectName);
+                    if (dep != null)
+                    {
+                        text.WriteLine($"   \"{p.Name}.csproj\" -> \"{dep.Name}.csproj\"");
+                    }
+                }
+            }
+        }
+        text.WriteLine("}");
+        return text.ToString();
+    }
+
+    /// <summary>
+    /// Ripped from https://stackoverflow.com/a/54275102
+    /// </summary>
+    /// <param name="path">Fully qualified path to the file you want to open</param>
+    static void OpenWithDefaultProgram(string path)
+    {
+        using Process fileopener = new Process();
+
+        fileopener.StartInfo.FileName = "explorer";
+        fileopener.StartInfo.Arguments = "\"" + path + "\"";
+        fileopener.Start();
+    }
+
+}
